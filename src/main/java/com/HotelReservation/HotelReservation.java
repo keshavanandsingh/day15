e3af9433a97668cc5953c993604431f7dcc7f333
keshavanandsingh <keshavanand22@gmail.com>
2020-10-12 00:31:32 +0530

    added

---
+++ src/main/java/com/HotelReservation/HotelReservation.java
@@ -0,0 +1,14 @@
+package com.HotelReservation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HotelReservation {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
